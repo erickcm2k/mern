28092f0f34237422cafb1f8dc3720f94261c59c7
Erick Castañeda Martínez <erickce40@gmail.com>
2020-12-11 17:32:13 -0600

    adding maps integration

--- mern-academind/main-project/frontend/src/places/components/PlaceItem.js
+++ mern-academind/main-project/frontend/src/places/components/PlaceItem.js
@@ -1,25 +1,60 @@
 import React from "react";
 import Card from "../../shared/components/UIElements/Card/Card";
+import Button from "../../shared/components/formelements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
 import "./PlaceItem.css";
 const PlaceItem = (props) => {
+  const [showMap, setShowMap] = React.useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+  const closeMapHandler = () => setShowMap(false);
+
   return (
-    <li className="place-item">
-      <Card className="place-item__content">
-        <div className="place-item__image">
-          <img src={props.image} alt={props.title} />
+    <>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+          <Map center={props.coordinates} zoom={16} />
+          {/* <iframe
+            title="maps"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.6173560726966!2d-73.98785308494105!3d40.74844454332273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9b3117469%3A0xd134e199a405a163!2sEmpire%20State%20Building!5e0!3m2!1ses-419!2smx!4v1607720392071!5m2!1ses-419!2smx"
+            
+
+            width="100%"
+            height="100%"
+            frameborder="0"
+            aria-hidden="false"
+            tabindex="0"
+          ></iframe> */}
         </div>
-        <div className="place-item__info">
-          <h2>{props.title}</h2>
-          <h3>{props.address}</h3>
-          <p>{props.description}</p>
-        </div>
-        <div className="place-item__actions">
-          <button>View On Map</button>
-          <button>Edit</button>
-          <button>Delete</button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div className="place-item__image">
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={openMapHandler}>
+              View On Map
+            </Button>
+            <Button to={`places/${props.id}`}>Edit</Button>
+            <Button danger>Delete</Button>
+          </div>
+        </Card>
+      </li>
+    </>
   );
 };
 
--- mern-academind/main-project/frontend/src/places/pages/NewPlace.js
+++ mern-academind/main-project/frontend/src/places/pages/NewPlace.js
@@ -1,6 +1,11 @@
 import React from "react";
-
+import Input from "../../shared/components/formelements/Input";
+import "./NewPlace.css";
 const NewPlace = () => {
-  return <h1>NewPlace work!</h1>;
+  return (
+    <form action="" className="place-form">
+      <Input type="text" label="Title" element="input" />
+    </form>
+  );
 };
 export default NewPlace;
